f5ee8a4cb762c43ce25543d1917f64b74d06aa58
AylenRodriguezISFT38 <Aylen12rodriguez@gmail.com>
2024-09-24 19:56:13 -0300

    Espaciado

--- Parcial1/Parcial1/Program.cs
+++ Parcial1/Parcial1/Program.cs
@@ -12,6 +12,7 @@ namespace Parcial1
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
         }
     }
 }
